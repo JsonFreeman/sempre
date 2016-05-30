bd31e325495013a71fee56d80359184cdcee8c7c
gpaka <sspkpl@stanford.edu>
2016-05-30 00:07:51 -0700

    Change running file so it can take different data files

--- runparse.sh
+++ runparse.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./run @mode=simple -Grammar.inPaths data/new.grammar -Dataset.inPaths train:data/unlabeled.examples -Learner.maxTrainIters 0 -SimpleLexicon.inPaths data/taxes.lexicon -typeInference -valueEvaluator ApproximateStringValueEvaluator | (python3 clean.py)
+./run @mode=simple -Grammar.inPaths data/george.grammar -Dataset.inPaths train:data/$1.examples -Learner.maxTrainIters 0 -SimpleLexicon.inPaths data/taxes.lexicon -typeInference -valueEvaluator ApproximateStringValueEvaluator # | (python3 clean.py)
